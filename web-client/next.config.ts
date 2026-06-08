06e760ce3a313f7617f8739ddf81d49c10966527
nguyenthithanhsuong <22521268@gm.uit.edu.vn>
2026-06-08 23:59:33 +0700

    Fix Multiple Sentry related bug

--- web-client/next.config.ts
+++ web-client/next.config.ts
@@ -7,11 +7,15 @@ loadEnv({ path: resolve(process.cwd(), "../.env") });
 loadEnv({ path: resolve(process.cwd(), ".env.local"), override: true });
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   transpilePackages: ["@greenplus/supabase-shared"],
 };
 
 export default withSentryConfig(nextConfig, {
   silent: !process.env.CI,
+
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT_CLIENT,
+
+  authToken: process.env.SENTRY_AUTH_TOKEN,
 });
