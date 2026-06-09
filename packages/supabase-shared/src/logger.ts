b02f9040c86a0378b010b9fe7f25035fa41a2819
nguyenthithanhsuong <22521268@gm.uit.edu.vn>
2026-06-09 13:51:49 +0700

    fix logger

--- packages/supabase-shared/src/logger.ts
+++ packages/supabase-shared/src/logger.ts
@@ -32,7 +32,7 @@ class BetterStackLogger {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          Authorization: this.sourceToken!,
+          Authorization: `Bearer ${this.sourceToken}`,
         },
         body: JSON.stringify({
           dt: new Date().toISOString(),
