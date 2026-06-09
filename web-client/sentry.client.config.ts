994eb8a2f9f012623b87eb76fa52d8ae1facec47
nguyenthithanhsuong <22521268@gm.uit.edu.vn>
2026-06-09 16:14:05 +0700

    Fix to improve Sentry

--- web-client/sentry.client.config.ts
+++ web-client/sentry.client.config.ts
@@ -4,5 +4,6 @@ Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   tracesSampleRate: 1.0,
   enabled: Boolean(process.env.NEXT_PUBLIC_SENTRY_DSN),
-  serverName: "web-client",
+  environment: process.env.NODE_ENV,
+  release: process.env.NEXT_PUBLIC_GITHUB_SHA,
 });
--- web-client/sentry.edge.config.ts
+++ web-client/sentry.edge.config.ts
@@ -2,7 +2,5 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  tracesSampleRate: 0.1,
-  enabled: Boolean(process.env.NEXT_PUBLIC_SENTRY_DSN),
-  serverName: "web-client",
+  tracesSampleRate: 1.0,
 });
